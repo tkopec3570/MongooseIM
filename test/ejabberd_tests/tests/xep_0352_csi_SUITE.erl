541a7e51bb84f768c625190464660c648d85991b
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-03-10 14:58:44 +0100

    add test cases where alice sends msg after becoming inactive

--- test/ejabberd_tests/tests/xep_0352_csi_SUITE.erl
+++ test/ejabberd_tests/tests/xep_0352_csi_SUITE.erl
@@ -9,7 +9,10 @@ all() ->
 
 groups() ->
     [{basic, [], [alice_is_inactive_and_no_stanza_arrived,
-                  alice_gets_msgs_after_activate]}].
+                  alice_gets_msgs_after_activate,
+                  bob_does_not_get_msgs_from_inactive_alice,
+                  bob_gets_msgs_from_aclie_after_she_is_active_back,
+                  bob_and_alice_get_msgs_from_each_other_after_alice_is_active]}].
 
 suite() ->
     escalus:suite().
@@ -48,9 +51,51 @@ alice_gets_msgs_after_activate(Config) ->
         %%When client becomes active again
         escalus:send(Alice, csi_stanza(<<"active">>)),
 
-        Msg = escalus:wait_for_stanza(Alice),
-        escalus:assert(is_chat_message, Msg)
+        then_client_receives_message(Alice)
     end).
+
+bob_does_not_get_msgs_from_inactive_alice(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
+        given_client_is_inactive_but_sends_message(Alice, Bob),
+
+        escalus_assert:has_no_stanzas(Bob)
+    end).
+
+bob_gets_msgs_from_aclie_after_she_is_active_back(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
+        given_client_is_inactive_and_message_sent(Alice, Bob),
+
+        escalus:send(Alice, escalus_stanza:chat_to(Bob, <<"Hi, Bob">>)),
+
+        %%When client becomes active again
+        escalus:send(Alice, csi_stanza(<<"active">>)),
+
+        then_client_receives_message(Alice),
+        then_client_receives_message(Bob)
+    end).
+
+bob_and_alice_get_msgs_from_each_other_after_alice_is_active(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
+        given_client_is_inactive_but_sends_message(Alice, Bob),
+
+        %%When client becomes active again
+        escalus:send(Alice, csi_stanza(<<"active">>)),
+
+        then_client_receives_message(Bob)
+    end).
+
+
+given_client_is_inactive_but_sends_message(Alice, Bob) ->
+    %%Given
+    given_client_is_inactive_and_message_sent(Alice, Bob),
+
+    escalus:send(Alice, escalus_stanza:chat_to(Bob, <<"Hi, Bob">>)),
+    timer:sleep(1).
+
+
+then_client_receives_message(Alice) ->
+    Msg = escalus:wait_for_stanza(Alice),
+    escalus:assert(is_chat_message, Msg).
 
 given_client_is_inactive_and_message_sent(Alice, Bob) ->
     %%Given
@@ -68,3 +113,4 @@ given_client_is_inactive(Alice) ->
 csi_stanza(Name) ->
     #xmlel{name = Name,
            attrs = [{<<"xmlns">>, <<"urn:xmpp:csi:0">>}]}.
+
